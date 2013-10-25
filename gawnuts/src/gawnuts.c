b1cbb8260cfd524f9ad8c2fcb73fea98739906db
Ataliba Teixeira <ataliba@ataliba.eti.br>
2013-10-24 22:09:19 -0200

    Adicionado o sorri no reconhecimento de fala no talker ( agora boa parte dos commits serão em portugues já que o talker é uma base em português.

--- gawnuts/src/gawnuts.c
+++ gawnuts/src/gawnuts.c
@@ -489,6 +489,7 @@ char type[10];
 switch(inpstr[strlen(inpstr)-1]) {
 	case '?': strcpy(type,"pergunta");  break;
 	case '!': strcpy(type,"exclama");  break;
+	case ':)': strcpy(type,"sorri"); break;
 	default : strcpy(type,"diz");
 	}
 sprintf(mess,"=> Voce %s: %s",type,inpstr);
